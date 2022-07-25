946a669fcbb5c1d0993bdb01419b8f06bfe69aad
Ygor Guilherme <ygorfsguilherme@gmail.com>
2022-07-25 18:49:04 -0300

    inicio do projeto

---
+++ src/main/java/br/org/sagr/commission/controller/IndexController.java
@@ -0,0 +1,12 @@
+package br.org.sagr.commission.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/test/java/br/org/sagr/commission/CommissionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.sagr.commission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
